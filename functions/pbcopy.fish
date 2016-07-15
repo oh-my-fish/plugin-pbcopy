bac325778d5ee8b4f1004838b37686177fc0cc7b
Derek Stavis <dekestavis@gmail.com>
2016-07-15 18:48:17 -0300

    don't define functions when on OS X

--- functions/pbcopy.fish
+++ functions/pbcopy.fish
@@ -1,3 +1,5 @@
-function pbcopy --description "Copy data from STDIN to the clipboard"
-  xsel --clipboard --input
+if not command -q pbcopy >/dev/null ^&1
+  function pbcopy --description "Copy data from STDIN to the clipboard"
+      xsel --clipboard --input
+  end
 end
--- functions/pbpaste.fish
+++ functions/pbpaste.fish
@@ -1,3 +1,5 @@
-function pbpaste  --description "Paste data from the Clipboard"
-  xsel --clipboard --output
+if not command -s pbpaste >/dev/null ^&1
+  function pbpaste --description "Paste data from the Clipboard"
+      xsel --clipboard --output
+  end
 end
